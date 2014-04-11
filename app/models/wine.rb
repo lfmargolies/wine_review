a40b9b7fd818ec47ad61fedcde4c80355f2120d5
Lee Margolies <lfmargolies@gmail.com>
2014-04-10 21:10:38 -0400

    HW Update

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,2 +1,6 @@
 class Wine < ActiveRecord::Base
+	validates :name, :year, :varietal, :winery, :country, presence: true
+validates :year, 
+    numericality: { greater_than_or_equal_to: 0 },
+    if: "year.present?"
 end
